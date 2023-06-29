7055ba42d283bb4b132d67a9c29c27f67ee2378b
Wisnu <wisnuarisetiadi@gmail.com>
2023-06-29 08:54:31 +0800

    Initial commit for go-hello-world
    29 Juni 2023

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	log.Println("Starting server at 5000...")
+	http.ListenAndServe(":5000", r)
+}
