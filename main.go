c46dc1eca52181716ede37d6492d87f0968b7115
Wisnu <wisnuarisetiadi@gmail.com>
2023-06-29 20:33:40 +0800

    Add ENV

--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World v.2023.06-29_18.54 From PROD"))
+		w.Write([]byte("Hello World v.2023.06-29_18.54 From " + os.Getenv("MODE")))
 	})
 	log.Println("Starting server at 5000...")
 	http.ListenAndServe(":5000", r)
